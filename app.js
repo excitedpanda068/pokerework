017e9c7b1e23b642bece67ddd6a5840237ed410a
excitedpanda068 <115201984+excitedpanda068@users.noreply.github.com>
2022-10-18 15:42:25 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+function popUp(i) {
+
+    switch(i) {
+        case 0:
+            var popup = document.getElementById("p0")
+            break
+        case 1:
+            var popup = document.getElementById("p1")
+            break
+        case 2:
+            var popup = document.getElementById("p2")
+            break
+        case 3:
+            var popup = document.getElementById("p3")
+            break
+        case 4:
+            var popup = document.getElementById("p4")
+            break
+        case 5:
+            var popup = document.getElementById("p5")
+            break
+        case 6:
+            var popup = document.getElementById("p6")
+            break
+        case 7:
+            var popup = document.getElementById("p7")
+            break
+        case 8:
+            var popup = document.getElementById("p8")
+            break
+        case 9:
+            var popup = document.getElementById("p9")
+            break
+        case 10:
+            var popup = document.getElementById("p10")
+            break
+        case 11:
+            var popup = document.getElementById("p11")
+            break
+        case 12:
+            var popup = document.getElementById("p12")
+            break
+        case 13:
+            var popup = document.getElementById("p13")
+            break
+        case 14:
+            var popup = document.getElementById("p14")
+            break
+        case 15:
+            var popup = document.getElementById("p15")
+            break
+        case 16:
+            var popup = document.getElementById("p16")
+            break
+        case 17:
+            var popup = document.getElementById("p17")
+            break
+        case 18:
+            var popup = document.getElementById("p18")
+            break
+        case 19:
+            var popup = document.getElementById("p19")
+            break
+        case 20:
+            var popup = document.getElementById("p20")
+            break
+        case 21:
+            var popup = document.getElementById("p21")
+            break
+        case 22:
+            var popup = document.getElementById("p22")
+            break
+        case 23:
+            var popup = document.getElementById("p23")
+            break
+
+    }
+    popup.classList.toggle("show")
+}
